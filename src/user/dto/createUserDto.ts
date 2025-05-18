52c6f845cf217a8c6cc45a64f9c96c73523a354d
Huyz109 <phamqhuy2001@gmail.com>
2025-05-18 20:26:24 +0700

    validate pipe

--- src/user/dto/createUserDto.ts
+++ src/user/dto/createUserDto.ts
@@ -1,6 +1,12 @@
+import { IsEmail, IsNumberString, IsString } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNumberString()
     phoneNumber: string;
-    
 }
