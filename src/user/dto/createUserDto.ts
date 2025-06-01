e9fe6a25b84e7ba9eec25213c511755529a0eb5f
Huyz109 <phamqhuy2001@gmail.com>
2025-06-01 17:34:35 +0700

    swagger document

--- src/user/dto/createUserDto.ts
+++ src/user/dto/createUserDto.ts
@@ -1,13 +1,35 @@
 import { PartialType } from "@nestjs/mapped-types";
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString } from "class-validator";
 
 export class CreateUserDto {
+    @ApiProperty()
     @IsString()
     name: string;
 
+    @ApiProperty()
     @IsEmail()
     email: string;
 
+    @ApiProperty()
+    @IsString()
+    password: string;
+}
+
+export class CreateUserResponseDto {
+    @ApiProperty()
+    @IsString()
+    id: string;
+
+    @ApiProperty()
+    @IsString()
+    name: string;
+    
+    @ApiProperty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
     @IsString()
     password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
 import { CommentService } from './../comment/comment.service';
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
-import { CreateUserDto, UpdateUserDto } from './dto/createUserDto';
+import { CreateUserDto, CreateUserResponseDto, UpdateUserDto } from './dto/createUserDto';
 import { UserService } from './user.service';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
@@ -10,26 +11,42 @@ export class UserController {
         private readonly commentService: CommentService
     ) {}
 
+    @ApiOperation({summary: "Get all user"})
+    @ApiOkResponse()
     @Get('all')
     findAll() {
         return 'All users';
     };
 
+    @ApiOperation({summary: "Get user information by id"})
+    @ApiOkResponse()
     @Get(':id')
     findUserById(@Param("id") id: number) {
         return this.userService.findOneById(id);
     }
 
+    @ApiOperation({summary: "Create new user"})
+    @ApiCreatedResponse({
+        description: "User created successfully",
+        type: CreateUserResponseDto
+    })
     @Post()
     create(@Body() body: CreateUserDto) {
         return this.userService.createUser(body);
     }
 
+    @ApiOperation({summary: "Get comment by user id"})
+    @ApiOkResponse()
     @Get(':id/comment')
     getCommentByUserId(@Param('id') id:string) {
         return this.commentService.findUserComments(id);
     }
 
+    @ApiOperation({summary: "Update user"})
+    @ApiOkResponse({
+        description: "User updated successfully",
+        type: CreateUserResponseDto
+    })
     @Put(':id')
     updateUser(@Param('id') id:number, @Body() body: UpdateUserDto) {
         return this.userService.updateUser(id, body);
